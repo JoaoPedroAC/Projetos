4107714bb25c49d422200941fbbe496275cc69ae
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2021-09-25 12:27:18 -0300

    1.3
    Endireitei um pouco o responsivo

--- Cadastro/script.js
+++ Cadastro/script.js
@@ -1,3 +1,8 @@
 // createElement('') -> cria uma tag
 //createTextNode('') -> cria texto dentro de um nó(tag) ou area especificada
 //appendChild() -> vai anexar como ultima filha dentro de um nó(tag)
+
+//LOGICA
+/*
+eu preciso casdastrar nome, email usando os botões e fazer os dados aparecerem no textarea e preciso tbm fzr um relogio atualizando em tempo real.
+ */
