d8c412812bfc59e6740e73e4b4881c1d820daf87
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2021-09-25 00:51:36 -0300

    1.1
    Iniciei um projeto novo com foco em JS

---
+++ Cadastro/README.md
@@ -0,0 +1,2 @@
+## SOBRE O PROJE
+O Projeto "Cadastro" tem como propósito treinar DOM e interações simples com JS
---
+++ Cadastro/script.js
@@ -0,0 +1,3 @@
+// createElement('') -> cria uma tag
+//createTextNode('') -> cria texto dentro de um nó(tag) ou area especificada
+//appendChild() -> vai anexar como ultima filha dentro de um nó(tag)
