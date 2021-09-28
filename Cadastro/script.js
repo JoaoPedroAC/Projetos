70ac1a446bcfb7f29373a4470c28eefa19c2b914
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2021-09-27 23:50:31 -0300

    1.5
    coloquei o suposto relogio e melhorei o código

--- Cadastro/script.js
+++ Cadastro/script.js
@@ -11,42 +11,47 @@ const nome = document.getElementById('nome');
 const email = document.getElementById('email');
 const cadastrar = document.getElementById('cadastrar');
 const texto = document.getElementById('AreaTexto');
+const tempo = document.getElementById('timer');
 var n;
 var e;
 var numero = 0;
+var msg = new Date();
+var t = 0
+let reg;
+// Aqui é o relógio
+tempo.innerHTML = `${msg.getDate()} / ${msg.getMonth()+1} / ${msg.getFullYear()}`
+
 // aqui quando apertar o bota vai pedir para cadastrar o nome
-Nome_Botao = () => {
+const nomeBotao = () => {
     nome.onmouseout = () => {
         nome.style.backgroundColor = 'rgb(241, 236, 236)';
     };
     nome.style.backgroundColor = "rgb(223, 219, 219)";
     n = prompt('Escreva seu nome aqui')
-
-}
+};
 
 
 // aqui quando apertar o bota vai pedir para cadastrar o e-mail
-Email_Botao = () => {
+const emailBotao = () => {
     email.onmouseout = () => {
         email.style.backgroundColor = 'rgb(241, 236, 236)';
     };
     email.style.backgroundColor = "rgb(223, 219, 219)";
     e = prompt('Escreva seu e-mail aqui') + '\n';
-
 }
 
 
 // Ao clicar nisso vai trazer para dentro do textarea o conteudo informado
-Cadastrar_Botao = () => {
+const cadastrarBotao = () => {
     cadastrar.onmouseout = () => {
         cadastrar.style.backgroundColor = 'rgb(241, 236, 236)';
     };
     cadastrar.style.backgroundColor = "rgb(223, 219, 219)";
     // sem isso ele vai escrever uma vez e vai apagar
-    var reg = `ID do usuário: ${numero++} \n`;
-    reg += `Nome de usuário: ${n} \n`;
+    reg += `ID do usuário: ${numero++} \n`;
+    reg += `Nome do usuário: ${n} \n`;
     reg += `E-mail do usuário: ${e} \n`;
-    texto.value += reg;
+    texto.innerHTML = reg;
+    console.log(`Cadastrado em: ${msg.getDate()} / ${msg.getMonth()+1} / ${msg.getFullYear()} ${t++}`);
 
-
-}
+};
