0d8e9f8c7cfa104374ea5d4554b8151f9ccf272f
João Pedro de Almeida <joaopedro.alm.c@gmail.com>
2021-10-12 12:03:44 -0300

    0.7
    incrementei o reultado aparecer no total do html e add placeholder nos outros inputs

--- Calculator-Tip/Script/script.js
+++ Calculator-Tip/Script/script.js
@@ -7,28 +7,36 @@ const cinquenta = document.getElementById('cinquenta');
 const custom = document.getElementById('botao');
 const resetar = document.getElementsByClassName("r");
 const person = document.getElementById('pessoas');
+const tipCaunt = document.querySelector('#tipCaunt');
+const totalCaunt = document.querySelector('#totalCaunt');
 
-let tipCaunt = document.querySelector('#tipCaunt');
+
 let conta;
 let pessoas;
-let costumatizar;
+let custumatizar;
 
+// OBS = result2 está colculando errado, quando só number ele retorna errado o resultado e quando flutuado ele retorna NaN
 
 // 5%
 function cincoPorcento() {
-    // vai setar as cores ao clicar
+    // vai setar as cores ao clicar e ao sair de cima
     cinco.onmouseout = function() { cinco.style.backgroundColor = 'hsl(183, 100%, 15%)'; }
     cinco.style.backgroundColor = 'rgb(100, 202, 168)';
 
     // vai fazer  o calculo
     conta = bill.value;
     pessoas = person.value;
-    let result1 = parseFloat(conta * 0.05 / pessoas);
-    tipCaunt.innerHTML = result1.toFixed(2);
+    // tip amount
+    let result1 = conta * 0.05 / pessoas;
+    tipCaunt.innerHTML = parseFloat(result1).toFixed(2);
+    // total
+    let result2 = (result1 + conta) / pessoas;
+    totalCaunt.innerHTML = parseFloat(result2).toFixed(2);
+
 };
 // 10%
 function dezPorcento() {
-    // vai setar as cores ao clicar
+    // vai setar as cores ao clicar e ao sair de cima
     dez.onmouseout = function() { dez.style.backgroundColor = 'hsl(183, 100%, 15%)'; }
     dez.style.backgroundColor = 'rgb(100, 202, 168)';
 
@@ -40,7 +48,7 @@ function dezPorcento() {
 };
 // 15%
 function quinzePorcento() {
-    // vai setar as cores ao clicar
+    // vai setar as cores ao clicar e ao sair de cima
     quinze.onmouseout = function() { quinze.style.backgroundColor = 'hsl(183, 100%, 15%)'; }
     quinze.style.backgroundColor = 'rgb(100, 202, 168)';
 
@@ -52,7 +60,7 @@ function quinzePorcento() {
 };
 // 25%
 function vinteCincoPorcento() {
-    // vai setar as cores ao clicar
+    // vai setar as cores ao clicar e ao sair de cima
     vinteCinco.onmouseout = function() { vinteCinco.style.backgroundColor = 'hsl(183, 100%, 15%)'; }
     vinteCinco.style.backgroundColor = 'rgb(100, 202, 168)';
 
@@ -64,27 +72,27 @@ function vinteCincoPorcento() {
 };
 // 50%
 function cinquentaPorcento() {
-    // vai setar as cores ao clicar
+    // vai setar as cores ao clicar e ao sair de cima
     cinquenta.onmouseout = function() { cinquenta.style.backgroundColor = 'hsl(183, 100%, 15%)'; }
     cinquenta.style.backgroundColor = 'rgb(100, 202, 168)';
 
     // vai fazer  o calculo
     conta = bill.value;
     pessoas = person.value;
-    let result1 = parseFloat(conta * 0.1 / pessoas);
+    let result1 = parseFloat(conta * 0.15 / pessoas);
     tipCaunt.innerHTML = result1.toFixed(2);
 };
 // Numero costumatizado
 function custumerPorcent() {
-    // vai setar as cores ao clicar
+    // vai setar as cores ao clicar e ao sair de cima
     custom.onmouseout = function() { custom.style.backgroundColor = 'rgb(255,255,255)'; }
     custom.style.backgroundColor = 'rgb(100, 202, 168)';
 
     // vai fazer  o calculo
-    costumatizar = custom.value;
+    custumatizar = custom.value;
     conta = bill.value;
     pessoas = person.value;
-    let result1 = parseFloat((conta * (costumatizar / 100)) / pessoas);
+    let result1 = parseFloat((conta * (custumatizar / 100)) / pessoas);
     tipCaunt.innerHTML = result1.toFixed(2);
 };
 // vai resetar tudo
